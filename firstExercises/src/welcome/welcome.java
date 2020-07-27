55e2e99c6db21e2aa97410380f1c140f4bb79010
Oscar <aoholloway@hotmail.com>
2020-07-27 07:29:12 -0500

    beginning exercises

---
+++ firstExercises/src/welcome/welcome.java
@@ -0,0 +1,15 @@
+package welcome;
+
+public class welcome {
+	public static void main(String[] args) {
+		//Displays message by printing one line of text
+		System.out.println("Welcome to Java");
+		threeWelcomeMessages();
+	}
+	
+	public static void threeWelcomeMessages(){
+		System.out.println("Programming is Fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
